45245da176f47ef6d71760ca0cc1fff1efd750ff
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-09-12 01:35:31 +0600

    ---

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,7 +53,15 @@
                 $imageAlt = $profileSettings->image_alt_text ?: 'Profile Image';
                 
             @endphp
-            <svg class="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="--bg-color: {{ $profileSettings->background_color }}; --border-color: {{ $profileSettings->border_color }}; --shadow-color: {{ $profileSettings->shadow_color }}; --shadow-opacity: {{ $profileSettings->shadow_opacity / 100 }};" data-timestamp="{{ time() }}" id="profile-svg">
+            @php
+                $shadowHex = ltrim($profileSettings->shadow_color ?? '#000000', '#');
+                $sr = hexdec(substr($shadowHex, 0, 2));
+                $sg = hexdec(substr($shadowHex, 2, 2));
+                $sb = hexdec(substr($shadowHex, 4, 2));
+                $sop = ($profileSettings->shadow_opacity ?? 30) / 100;
+                $shadowFilter = "drop-shadow(0 8px 16px rgba($sr, $sg, $sb, $sop))";
+            @endphp
+            <svg class="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="--bg-color: {{ $profileSettings->background_color }}; --border-color: {{ $profileSettings->border_color }}; --shadow-color: {{ $profileSettings->shadow_color }}; --shadow-opacity: {{ $profileSettings->shadow_opacity / 100 }}; filter: {{ $shadowFilter }};" data-timestamp="{{ time() }}" id="profile-svg">
                 <mask id="mask0" mask-type="alpha">
                     <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z"/>
                 </mask>
